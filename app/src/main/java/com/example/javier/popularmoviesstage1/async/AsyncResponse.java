8d0a8e68d3f4daa4a788aa4faa56e81598483451
Javier Godino <jgodort.software@gmail.com>
2016-05-29 13:42:27 +0200

    Starting Stage 2

--- app/src/main/java/com/example/javier/popularmoviesstage1/async/AsyncResponse.java
+++ app/src/main/java/com/example/javier/popularmoviesstage1/async/AsyncResponse.java
@@ -17,7 +17,7 @@ public interface AsyncResponse {
     void onPostExecuteDelegate(List<MovieEntity> results);
 
     void onPreExecute();
-    void processMovies(List<MovieEntity> results);
+
 
 }
 
